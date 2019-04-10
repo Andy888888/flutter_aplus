1e71114db4b5821d49210e939d88198913f12ce8
燕文强 <yanwq@centaline.com.cn>
2019-04-10 18:52:33 +0800

    update request.dart

--- lib/network/request.dart
+++ lib/network/request.dart
@@ -4,11 +4,33 @@ class RequestManager {
   Dio _dio;
 
   RequestManager() {
-    // 或者通过传递一个 `options`来创建dio实例
     BaseOptions _options = new BaseOptions(
         baseUrl: "https://www.xx.com/api",
         connectTimeout: 5000,
         receiveTimeout: 3000);
-    _dio = new Dio( _options);
+    _dio = new Dio(_options);
+
+    _dio.interceptors.add(InterceptorsWrapper(
+        onRequest: (RequestOptions options) {
+          int check = DateTime.now().millisecondsSinceEpoch;
+          options.headers['version'] = '1.0.0';
+          options.headers['check'] = check;
+        },
+        onResponse: (Response response) {},
+        onError: (DioError error) {
+          int statusCode = error.response.statusCode;
+          // 500、400、404 ...
+        }));
+
+//    Future<dynamic> ss = request();
+  }
+
+  Future<dynamic> request() async {
+    Response response = await _dio.post(
+      '/test',
+      data: {"id": 12, "name": "xx"},
+    );
+
+    return response.data;
   }
 }
