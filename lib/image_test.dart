556726c20347c9b44b00862f56ecf40c01dba21c
燕文强 <yanwq@centaline.com.cn>
2019-05-10 18:24:39 +0800

    update image_test

--- lib/image_test.dart
+++ lib/image_test.dart
@@ -14,11 +14,21 @@ class StackImageState extends State<StackImages> {
   @override
   Widget build(BuildContext context) {
     var widgets = <Widget>[];
+    images.add('http://b.hiphotos.baidu.com/image/pic/item/32fa828ba61ea8d3fcd2e9ce9e0a304e241f5803.jpg');
+    images.add('http://e.hiphotos.baidu.com/image/pic/item/b8014a90f603738d6d8d0d65bd1bb051f919ecb6.jpg');
+    images.add('http://a.hiphotos.baidu.com/image/pic/item/38dbb6fd5266d01681fbe0859d2bd40734fa35b8.jpg');
+    images.add('http://e.hiphotos.baidu.com/image/pic/item/b03533fa828ba61ef47bb6824b34970a314e5981.jpg');
+    images.add('http://d.hiphotos.baidu.com/image/pic/item/a8014c086e061d95c1df499571f40ad163d9ca86.jpg');
+    images.add('http://d.hiphotos.baidu.com/image/pic/item/f703738da977391200fafbccf2198618377ae2bb.jpg');
+    images.add('http://e.hiphotos.baidu.com/image/pic/item/9f2f070828381f302d7ff404a3014c086e06f03d.jpg');
 
     for (int i = 0; i <= index; i++) {
-      var pos = i - (i ~/ 103) * 103;
+      var pos = i % images.length;
       widgets.add(new Container(
-          child: new Image.asset("images/${pos}.jpg", fit: BoxFit.cover),
+//          child: new Image.asset("images/${pos}.jpg", fit: BoxFit.cover),
+          child: new Image.network(
+              images[pos],
+              fit: BoxFit.cover),
           padding: new EdgeInsets.only(top: i * 2.0)));
     }
 
